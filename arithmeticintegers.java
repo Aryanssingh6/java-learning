35dd9ab7b95edcafe208d277db212684f3b70dc2
Aryan <singharyan180327@gmail.com>
2025-08-06 19:42:47 +0530

    add new java code

--- arithmeticintegers.java
+++
@@ -1,7 +0,0 @@
-public class arithmeticintegers {
-    public static void main(String[] args) {                                                   
-            int x = 4, y = 7;
-            System.out.println("The value of x is git add " + x);
-            System.out.print(" and the value of y is " + y);
-        }
-    }   
---
+++ multiplevraiables.java
@@ -0,0 +1,6 @@
+public class multiplevraiables {
+    public static void main(String[] args) {                                                   
+            int x = 4,y = 7, z = 3;
+            System.out.print("The sum of "y+z);
+        }
+    }
