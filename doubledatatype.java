1c540e9f593964cf4f0a83256d65463f7684b610
Aryan <singharyan180327@gmail.com>
2025-08-14 23:12:10 +0530

    add new java code

--- doubledatatype.java
+++ doubledatatype.java
@@ -1,10 +1,5 @@
 public class doubledatatype {
     public static void main(String[] args) {
-        double a = 5;
-        double b = 10;
-        System.out.println(a+b);
-        System.out.println(a-b);
-        System.out.println(a*b);  
-        System.out.println(a/b);
+        
     }
 }
---
+++ percentageof5subjects.java
@@ -0,0 +1,11 @@
+public class percentageof5subjects {
+    public static void main(String[] args) {
+        double x1 = 91;
+        double x2 = 85;
+        double x3 = 78;
+        double x4 = 88;
+        double x5 = 95;
+        double p = (x1 + x2 + x3 + x4 + x5) / 5;
+        System.out.println(p);
+    }
+}
